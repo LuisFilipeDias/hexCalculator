92f8f3b4822ce20078640cbba9a776ee5c04bf27
Luis Filipe Dias <luisfiliperdias@gmail.com>
2015-12-20 17:03:24 +0000

    Refactoring Calculator class, to use a LinkedList instead of a mix of variables and arrays

--- app/src/main/java/com/toam/android/hexcalculator/Calculator.java
+++ app/src/main/java/com/toam/android/hexcalculator/Calculator.java
@@ -1,5 +1,7 @@
 package com.toam.android.hexcalculator;
 
+import java.util.LinkedList;
+
 /**
  * Created by luisfilipedias on 13-12-2015.
  *
@@ -17,6 +19,17 @@ public class Calculator {
     private String display_copy;
     private char old_op;
 
+    class Parcel {
+        /* op = 'v' stands for value, otherwise they are actual operations */
+        char op;
+        /* in case op != 'v' value should be null */
+        double value;
+    }
+
+    private LinkedList <Parcel> q;
+    /* auxiliar linked list for calculation */
+    private LinkedList <Parcel> calc_q;
+
     /*
     these values are used to compute the result, when a * or / operation appears
         this is necessary because of precedence
@@ -25,43 +38,149 @@ public class Calculator {
     private double old_result;
     private double mlt_result;
     private double old_val;
-    private boolean multy;
+    private boolean showResult;
 
     public Calculator() {   
         /* init with base 10 */
         this.base = 10;
-        this.display = "";
-        this.update_values = false;
-        this.value = new double[Utils.MAX_DIGITS];
-        this.position = 0;
-        /* s is for start -> means result is the value itself */
-        this.op = 's';
-        this.old_op = 's';
-        this.multy = false;
+        /* show result only after first operation */
+        this.showResult = false;
+        this.q = new LinkedList<Parcel>();
+        this.calc_q = new LinkedList<Parcel>();
     }
 
     public void addKey(int key){
-        /* if update_values, push value to last_value before computing new value */
-        /*if(update_values) {
-            this.update_values = false;
-            this.display += this.op;
-        }*/
+        Parcel prc = new Parcel();
 
-        //computeResult();
-
-        // must multiplicate for the base
         this.curr_val = this.curr_val * this.base + key;
-        this.display += key;
-        this.display_copy = this.display;
+
+        /* insert value to queue */
+        prc.op = 'v';
+        prc.value = this.curr_val;
+
+        /* if queue isn't empty */
+        if(!this.q.isEmpty())
+        /* pop older, not updated value, then push new value */
+            if(this.q.peek().op == 'v')
+                this.q.pop();
+
+        this.q.push(prc);
+
         computeResult();
-//        this.setDisplay(this. + this.op, Double.toString(this.result));
+        refreshDisplay();
+    }
+
+    private void computeResult(){
+
+        /* e as in empty */
+        char prev_op = 'e';
+
+        for(int i = 0; i < this.q.size(); i++){
+
+            /* take values in the contrary order */
+            int idx = this.q.size()- i - 1;
+
+            char   l_op  = this.q.get(idx).op;
+            double l_val = this.q.get(idx).value;
+
+
+            if(l_op != 'v') {
+                prev_op = l_op;
+                this.showResult = true;
+                continue;
+            } else if(prev_op == 'e') {
+                /* in this case it's easy, no history, result is the value */
+                this.result = l_val;
+                continue;
+            }
+
+            switch(prev_op){
+                case '+':
+                    this.result = this.result + l_val;
+                    this.mlt_result = 1;
+                    break;
+                case '-':
+                    this.result = this.result - l_val;
+                    break;
+            }
+
+        }
+        this.sub_display = Double.toString(this.result);
+
+        /*
+        switch(this.op){
+             case s we don't want to print the result, go back
+            case 's':
+                this.result = this.curr_val;
+                this.mlt_result = 1;
+                return;
+            case '+':
+                this.multy = false;
+                this.old_result = this.result;
+                this.result = this.result + this.curr_val;
+                this.mlt_result = 1;
+                break;
+            case '-':
+                this.multy = false;
+                this.old_result = this.result;
+                this.result = this.result - this.curr_val;
+                this.mlt_result = 1;
+                break;
+            case '*':
+                 save last operator
+                 save last value
+                if( (this.old_op == '+' || this.old_op == '-' || this.old_op == 's') && !this.multy) {
+                    this.mlt_result = this.old_val;
+                    this.multy = true;
+                }
+
+                this.mlt_result = this.mlt_result * this.curr_val;
+                this.result = this.mlt_result;
+
+                switch (this.old_op){
+                    case '+':
+                        this.result = this.old_result + this.mlt_result;
+                        break;
+                    case '-':
+                        this.result = this.old_result - this.mlt_result;
+                        break;
+                }
+                break;
+            case '/':
+                if( (this.old_op == '+' || this.old_op == '-' || this.old_op == 's') && !this.multy) {
+                    this.mlt_result = this.old_val;
+                    this.multy = true;
+                }
+
+                this.mlt_result = this.mlt_result / this.curr_val;
+                this.result = this.mlt_result;
+
+                switch (this.old_op){
+                    case '+':
+                        this.result = this.old_result + this.mlt_result;
+                        break;
+                    case '-':
+                        this.result = this.old_result - this.mlt_result;
+                        break;
+                }
+                break;
+        }
+        this.sub_display = Double.toString(this.result);*/
     }
 
     public void setOperation(char op){
+        Parcel prc = new Parcel();
 
-        insertToList();
+        /* insert operation to queue */
+        prc.op = op;
+        prc.value = 0;
+        this.q.push(prc);
+
+        /* value has been pushed, reset it */
+        this.curr_val = 0;
+
         /* create a copy of op, to use when a * or / is called */
-        if(this.op == '+' || this.op == '=')
+/*        if(this.op == '+' || this.op == '=')
             this.old_op = this.op;
 
         switch(op) {
@@ -81,76 +200,30 @@ public class Calculator {
                 this.op = op;
                 break;
         }
+*/
+        refreshDisplay();
     }
 
-    private void insertToList(){
-        this.old_val = this.curr_val;
-        this.value[this.position] = this.curr_val;
-        this.curr_val = 0;
-        this.position += 1;
-    }
 
-    private void computeResult(){
-        switch(this.op){
-            /* case s we don't want to print the result, go back */
-            case 's':
-                this.result = this.curr_val;
-                this.mlt_result = 1;
-                return;
-            case '+':
-                this.multy = false;
-                this.old_result = this.result;
-                this.result = this.result + this.curr_val;
-                this.mlt_result = 1;
-                break;
-            case '-':
-                this.multy = false;
-                this.old_result = this.result;
-                this.result = this.result - this.curr_val;
-                this.mlt_result = 1;
-                break;
-            case '*':
-                /* save last operator */
-                /* save last value */
-                if( (this.old_op == '+' || this.old_op == '-' || this.old_op == 's') && !this.multy) {
-                    this.mlt_result = this.old_val;
-                    this.multy = true;
-                }
+    public void refreshDisplay() {
+        this.display = "";
 
-                this.mlt_result = this.mlt_result * this.curr_val;
-                this.result = this.mlt_result;
+        /* if is empty, there is nothing to display */
+        if(this.q.isEmpty())
+            return;
 
-                switch (this.old_op){
-                    case '+':
-                        this.result = this.old_result + this.mlt_result;
-                        break;
-                    case '-':
-                        this.result = this.old_result - this.mlt_result;
-                        break;
-                }
-                break;
-            case '/':
-                /* save last operator */
-                /* save last value */
-                if( (this.old_op == '+' || this.old_op == '-' || this.old_op == 's') && !this.multy) {
-                    this.mlt_result = this.old_val;
-                    this.multy = true;
-                }
+        for(int i = 0; i < this.q.size(); i++){
+            /* take values in the contrary order */
+            int idx = this.q.size()- i - 1;
 
-                this.mlt_result = this.mlt_result / this.curr_val;
-                this.result = this.mlt_result;
+            char   l_op  = this.q.get(idx).op;
+            double l_val = this.q.get(idx).value;
 
-                switch (this.old_op){
-                    case '+':
-                        this.result = this.old_result + this.mlt_result;
-                        break;
-                    case '-':
-                        this.result = this.old_result - this.mlt_result;
-                        break;
-                }
-                break;
+            if(l_op == 'v')
+                this.display += Double.toString(l_val);
+            else
+                this.display += l_op;
         }
-        this.sub_display = Double.toString(this.result);
     }
 
     public String getDisplay() {
@@ -158,7 +231,10 @@ public class Calculator {
     }
 
     public String getSubDisplay() {
-        return this.sub_display;
+        if(showResult)
+            return this.sub_display;
+        else
+            return "";
     }
 
     private void setBase(int base){
